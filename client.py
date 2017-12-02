aea59e599953e2c6ab61f4e2a2742c8f3bb905f7
paulsha <shanno06@hotmail.com>
2017-12-02 16:25:47 +0100

    Added file functions in Client

--- client.py
+++ client.py
@@ -1,26 +1,72 @@
 import SecurityService as sec_serv
-import requests
 import base64
 import json
+import requests
+import sys
 
+sec_serv_url = 'http://127.0.0.1:5001/'
+dir_serv_url = 'http://127.0.0.1:5002/'
 
 def main():
-#read user input for username, password
-	user_ID = input("Username: ")
-    user_PW = input("Password: ")
-	sec_url = 'http://127.0.0.1:5001/'
-+   dir_url = 'http://127.0.0.1:5002/'
-#invoke base64 encoding so not raw text	
-	encryption_ID = base64.urlsafe_b64encode(sec_serv.encrypt(user_ID, user_PW).encode()).decode()
-	JSON_auth_init = {'user_ID': user_ID, 'password': user_PW, 'encrypted_id': encrpytion_ID, 'server_ID': 'Directory'}
-	
-	#POST JSON
-	authReq = requests.post('http://127.0.0.1:5001/auth', json=JSON_auth_init)
-	encryption_Token = authReq.json()['token']
-    decryption_Token = json.loads(sec_serv.decrypt(base64.urlsafe_b64decode(encryption_Token).decode(), user_PW))
-	#Print to Client
-	print(decrpytion_Token)
-	
-#Def	
-if __name__ == "__main__":
+
+    # user login, each new client will be asked to log on, their details will then be stored in the sec_serv database
+    user_ID = input("Please Enter Username: ")
+    user_PW = input("Please Enter Pasec_servword: ")
+
+    encId = base64.urlsafe_b64encode(sec_serv.encrypt(user_ID, user_PW).encode()).decode()
+
+    print('Accesec_serving Security Service')
+    JSON_auth = {'user_ID': user_ID, 'pasec_servword': user_PW, 'encrypted_ID': encId, 'server_ID': 'File Server 1'}
+    print('Sending', JSON_auth)
+
+
+    authReq = requests.post(sec_serv_url+'auth', json=JSON_auth)
+    encryption_Token = authReq.json()['token']
+    decryption_Token = json.loads(sec_serv.decrypt(base64.urlsafe_b64decode(encToken).decode(), user_PW))
+    print(decToken)
+
+    list_open_files = []
+    connected = True
+
+#standard functions for file operations, parses file name and then performs required operation
+def read_file(list_open_files, filename):
+    for file in list_open_files:
+        if file['filename'] == filename:
+            return file['file_content']
+    return "File open with that name not found!"
+
+
+def write_file(list_open_files, filename, content):
+    for file in list_open_files:
+        if file['filename'] == filename:
+            file['file_content'] = content
+            updated_file = requests.post(dir_serv_url+"write", json=file)
+            return updated_file.text
+    return "File open with that name not found!"
+
+
+def open(list_open_files, filename):
+    for file in list_open_files:
+        if file['filename'] == filename:
+            return list_open_files, 'File already opened.'
+    open_file = requests.get(dir_serv_url + "open/" + filename)
+    list_open_files.append(open_file.json())
+    return list_open_files, 'File opened"
+
+
+def close(list_open_files, filename):
+    for file in list_open_files:
+        if file['filename'] == filename:
+            list_open_files.remove(file)
+            return list_open_files, "File succesec_servfully closed."
+    return list_open_files, "File open with that name not found!"
+
+
+def add(filename, filepath):
+    file = {'filename': filename, 'filepath': filepath}
+    post = requests.post(dir_serv_url+"add", json=file)
+    return post.text
+
+
+if __name__=="__main__":
     main()
