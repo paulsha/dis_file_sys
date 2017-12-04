7ab9a524db4a6b73cfade51417d53e0ffaec0f3b
paulsha <shanno06@hotmail.com>
2017-12-04 23:54:09 +0100

    Add File Functionality for Directory Service

--- dir_service.py
+++ dir_service.py
@@ -4,18 +4,39 @@ import SecurityService as sec_serv
 app = FlaskAPI(__name__)
 fs_url = 'http://127.0.0.1:'
 
-#series of functions to read,write,and get directory for dir_service
+#series of functions to read,write,add and get directory for dir_service
 
 @app.route('/write', methods=['POST'])
 def wf(filename):
-    for n in [7, 8]:
+    for n in [5007, 5008]:
         server_url = fs_url+str(n)+"/"+"write/"+filename
         in_directory = requests.post(server_url)
         print(in_directory.text)
         status_code = in_directory.status_code
         if status_code == 200:
+            return "File written"
+    return {'Unknown Error Occured'}
+
+@app.route('/open/<filename>', methods=['GET'])
+def rf(filename):
+    for n in [5007, 5008]:
+        server_url = fs_url+str(n)+"/"+"open/"+filename
+        in_directory = requests.get(server_url)
+        print(in_directory.text)
+        status_code = in_directory.status_code
+        if status_code == 200:
             return in_directory.json()
-    return {'Error:': 'no file found'}
+    return {'File not found on any server.'}
+
+@app.route('/add', methods=['POST'])
+def af():
+    file = request.json
+    for n in [5007, 5008]:
+        server_name = requests.get(file_server_url + str(n) + "/name")
+        if server_name.text == file['filepath']:
+            post = requests.post(file_server_url + str(n) + "/add", json=file)
+            return post.text
+    return 'No server'
 	
 @app.route('/get_dir/<filename>', methods=['GET'])
 def get_dir(filename):
@@ -25,18 +46,7 @@ def get_dir(filename):
     if status_code == 200:
         return in_directory.json()
     else:
-        return {'Error:': 'no file found.'}
-
-@app.route('/open/<filename>', methods=['GET'])
-def rf(filename):
-    for n in [7, 8]:
-        server_url = fs_url+str(n)+"/"+"open/"+filename
-        in_directory = requests.get(server_url)
-        print(in_directory.text)
-        status_code = in_directory.status_code
-        if status_code == 200:
-            return in_directory.json()
-    return {'Error:': 'no file found.'}
-
+        return {'File not found on any server.'}
+		
 if __name__=='__main__':
     app.run(port=5002)
